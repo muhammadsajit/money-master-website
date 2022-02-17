5eb484813cc69fc55cbeb989c9f61d24f1c566b1
muhammadsajit <sajit.du.niter@gmail.com>
2022-02-17 20:18:35 +0600

    savings calculate

--- js/money.js
+++ js/money.js
@@ -1,3 +1,6 @@
+
+//handle calculate event handler
+
 document.getElementById('calculate-button').addEventListener('click',function(){
     //get income field
      const incomeField=document.getElementById('income-input');
@@ -10,26 +13,26 @@ document.getElementById('calculate-button').addEventListener('click',function(){
      const foodField=document.getElementById('food-input');
     const newFoodValueText=foodField.value;
     const newFoodValue=parseFloat(newFoodValueText);
-//        //get rent field
+       //get rent field
     const rentField=document.getElementById('rent-input');
     const newRentValueText=rentField.value;
     const newRentValue=parseFloat(newRentValueText);
-//      //get cloth field
+     //get cloth field
      const clothesField=document.getElementById('cloth-input');
     const newClothesValueText=clothesField.value;
       const newClothesValue=parseFloat(newClothesValueText);
 
    
-//     //total expense calculate
+     //total expense calculate
       const totalExpense=document.getElementById('total-expense');
       const previousExpenseText= totalExpense.innerText;
       const previousExpense=parseFloat(previousExpenseText);
      const newExpense=previousExpense+newFoodValue+newRentValue+newClothesValue;
     totalExpense.innerText=newExpense;
-       //balnce calcute
+       //balance calculate
        const balance=document.getElementById('balance');
-            const previousBalanceText= balance.innerText;
-            const previousBalance=parseFloat(previousBalanceText);
+            // const previousBalanceText= balance.innerText;
+            // const previousBalance=parseFloat(previousBalanceText);
              const newBalance=newIncomeValue-newExpense;
              balance.innerText=newBalance;
     //clear income Field
@@ -45,4 +48,28 @@ document.getElementById('calculate-button').addEventListener('click',function(){
 
 
 
-})
+});
+//handle save event handler
+document.getElementById('save-button').addEventListener('click',function(){
+    //get income field
+    const incomeField=document.getElementById('income-input');
+    const newIncomeValueText=incomeField.value;
+    const newIncomeValue=parseFloat(newIncomeValueText);
+    
+    //get save field
+    const saveField=document.getElementById('save-input');
+       
+     const newSaveValueText=saveField.value;
+     const newSaveValue=parseFloat(newSaveValueText);
+     //calculate savings
+      const savings=document.getElementById('savings');
+    //   const previousSavingsText= savings.innerText;
+    //   const previousSavings=parseFloat(previousSavingsText);
+      const savingsPercentage= newSaveValue/parseFloat(100);
+     const newSavings=newIncomeValue*savingsPercentage;
+    savings.innerText= newSavings;
+
+    //clear save Field
+    saveField.value='';
+
+});
